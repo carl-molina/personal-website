d6257042f9558761d8004c1f9ace1eacb2ce1995
Carl Molina <carlandrew.molina@gmail.com>
2024-02-11 13:12:39 -0800

    added boilerplate for navbar

---
+++ src/Navbar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+/** Navbar: component for personal website.
+ *
+ *  Props: none
+ *  State: none
+ *
+ *  App -> Navbar
+ */
+
+function Navbar(): JSX.Element {
+
+    return (
+        <div className="Navbar">
+            <nav>
+                <NavLink to="/" className="Navbar-title">Carl Molina</NavLink>
+                <ul>
+                    <li><a href="./resume/CarlMolina_Resume.pdf" download="CarlMolina_Resume.pdf">Resume</a></li>
+                    <li><NavLink to="/companies">Projects</NavLink></li>
+                    <li><NavLink to="/jobs">Contact</NavLink></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+
+export default Navbar;
