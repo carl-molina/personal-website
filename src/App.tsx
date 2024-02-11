9acebc0b245139bc097dbabfb6bf2ddae7432ab2
Carl Molina <carlandrew.molina@gmail.com>
2024-02-11 00:21:34 -0800

    finished first draft of working parallax

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 
 import bg from "./images/IMG_1458.jpg";
+import fg from "./images/layered-peaks-haikei.svg";
+
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 /** Component for entire page.
  *
@@ -15,10 +18,43 @@ function App(): React.ReactElement {
   return (
     <div className="App">
       <main>
-        <img src={bg} alt="night-sky" className="bg-image"/>
+        <Parallax pages={2}>
+          <ParallaxLayer
+            offset={0}
+            speed={0.5}
+            factor={2.5}
+            style={{
+              backgroundImage: `url(${bg})`,
+              backgroundSize: 'cover',
+            }}
+          />
+          <ParallaxLayer
+            offset={1}
+            speed={1}
+            factor={1}
+            style={{
+              backgroundImage: `url(${fg})`,
+              backgroundSize: 'cover',
+            }}
+          >
+          </ParallaxLayer>
+        </Parallax>
       </main>
     </div>
   );
 };
 
 export default App;
+
+
+// old way:
+// <Parallax pages={2}>
+// <ParallaxLayer speed={1}>
+//   <h1>Test 1 layer</h1>
+//   <img src={bg} alt="night-sky" className="bg-image"/>
+// </ParallaxLayer>
+// <ParallaxLayer offset={1} speed={0.5}>
+//   <h1>Test 2 Layer</h1>
+//   <img src={fg} alt="layered-peaks" className="fg-image"/>
+// </ParallaxLayer>
+// </Parallax>
