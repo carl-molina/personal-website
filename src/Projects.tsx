92da2bccbad9f4bcac7df584ec2065edab0ede85
Carl Molina <carlandrew.molina@gmail.com>
2024-02-17 16:08:45 -0800

    Imported css and added classNames

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import "./Projects.css";
+import "./Projects.css";
 
 /** Projects: presentational component for projects.
  *
@@ -13,28 +13,37 @@ function Projects(): JSX.Element {
 
     return (
         <div>
-            <h2>
-                <a
-                    href="https://carlmolina-jobly.onrender.com/"
-                    target="_blank"
-                    rel="noopener">
-                    JOBLY
-                </a>
-            </h2>
-            <h2>
-                <a href="https://carl-molina-pixly.onrender.com/"
-                    target="_blank"
-                    rel="noopener">
-                    PIXLY
-                </a>
-            </h2>
-            <h2>
-                <a href="https://carl-molina-plant-app.onrender.com/"
-                    target="_blank"
-                    rel="noopener">
-                    Plant App
-                </a>
-            </h2>
+            <div className="Projects-container">
+                <h2>
+                    <a
+                        href="https://carlmolina-jobly.onrender.com/"
+                        target="_blank"
+                        rel="noopener"
+                        className="Projects-project">
+                        JOBLY
+                    </a>
+                </h2>
+            </div>
+            <div className="Projects-container">
+                <h2>
+                    <a href="https://carl-molina-pixly.onrender.com/"
+                        target="_blank"
+                        rel="noopener"
+                        className="Projects-project">
+                        PIXLY
+                    </a>
+                </h2>
+            </div>
+            <div className="Projects-container">
+                <h2>
+                    <a href="https://carl-molina-plant-app.onrender.com/"
+                        target="_blank"
+                        rel="noopener"
+                        className="Projects-project">
+                        Plant App
+                    </a>
+                </h2>
+            </div>
         </div>
     );
 }
