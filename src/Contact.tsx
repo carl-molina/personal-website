ba9879d22a0fd51afe51ae1307845ee0ee9036c9
Carl Molina <carlandrew.molina@gmail.com>
2024-02-18 19:42:30 -0800

    added div for Contact-container

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -13,13 +13,15 @@ function Contact(): JSX.Element {
 
     return (
         <>
-            <h5 className="Contact-email">carl@carlmolina.com</h5>
-            <div className="Contact-icons">
-                <h5><a href="https://www.linkedin.com/in/carlmolina/" className="nav-link" target="_blank" rel="noopener"><i className="fa-brands fa-linkedin"></i></a></h5>
-                <h5><a href="https://github.com/carl-molina" className="nav-link" target="_blank" rel="noopener"><i className="fa-brands fa-github"></i></a></h5>
-                <h5><a href="https://medium.com/@carlandrew.molina" className="nav-link" target="_blank" rel="noopener"><i className="fa-brands fa-medium"></i></a></h5>
-                <h5><a href="https://twitter.com/carlandmolina" className='nav-link' target="_blank" rel="noopener"><i className="fa-brands fa-x-twitter"></i></a></h5>
-                <h5><a href="mailto:carl@carlmolina.com" className="nav-link"><i className="fa-solid fa-envelope"></i></a></h5>
+            <div className="Contact-container">
+                <h5 className="Contact-email">carl@carlmolina.com</h5>
+                <div className="Contact-icons">
+                    <h5><a href="https://www.linkedin.com/in/carlmolina/" className="nav-link" target="_blank" rel="noopener"><i className="fa-brands fa-linkedin"></i></a></h5>
+                    <h5><a href="https://github.com/carl-molina" className="nav-link" target="_blank" rel="noopener"><i className="fa-brands fa-github"></i></a></h5>
+                    <h5><a href="https://medium.com/@carlandrew.molina" className="nav-link" target="_blank" rel="noopener"><i className="fa-brands fa-medium"></i></a></h5>
+                    <h5><a href="https://twitter.com/carlandmolina" className='nav-link' target="_blank" rel="noopener"><i className="fa-brands fa-x-twitter"></i></a></h5>
+                    <h5><a href="mailto:carl@carlmolina.com" className="nav-link"><i className="fa-solid fa-envelope"></i></a></h5>
+                </div>
             </div>
         </>
 
